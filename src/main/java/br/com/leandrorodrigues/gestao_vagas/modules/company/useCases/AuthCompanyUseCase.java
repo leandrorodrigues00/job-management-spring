182f23bf935e0c1591c384c646ba108951c87a02
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2024-12-22 12:32:24 -0300

    feat: jwt token creation

--- src/main/java/br/com/leandrorodrigues/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/leandrorodrigues/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -2,7 +2,10 @@ package br.com.leandrorodrigues.gestao_vagas.modules.company.useCases;
 
 import br.com.leandrorodrigues.gestao_vagas.modules.company.dto.AuthCompanyDTO;
 import br.com.leandrorodrigues.gestao_vagas.modules.company.repositories.CompanyRepository;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -13,16 +16,19 @@ import javax.naming.AuthenticationException;
 @Service
 public class AuthCompanyUseCase {
 
+    @Value("${security.token.secret}")
+    private String secretKey;
+
     @Autowired
     private CompanyRepository companyRepository;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public void execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
+    public String execute(AuthCompanyDTO authCompanyDTO) throws AuthenticationException {
         var company = this.companyRepository.findByUsername(authCompanyDTO.getUsername()).orElseThrow(
                 () -> {
-                    throw new UsernameNotFoundException("Company not found");
+                    throw new UsernameNotFoundException("Username/password incorrect");
                 }
         );
 
@@ -32,5 +38,10 @@ public class AuthCompanyUseCase {
             throw new AuthenticationException();
         }
 
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKey);
+        var token = JWT.create().withIssuer("javagas").withSubject(company.getId().toString()).sign(algorithm);
+
+        return token;
     }
 }
