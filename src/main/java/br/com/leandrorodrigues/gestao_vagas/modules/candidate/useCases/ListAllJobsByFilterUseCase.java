568c8abef1b69f8137682bb96f248243be2d4f05
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2024-12-30 09:30:52 -0300

    feat: UseCase: ListAllJobsByFilter

--- src/main/java/br/com/leandrorodrigues/gestao_vagas/modules/candidate/useCases/ListAllJobsByFilterUseCase.java
+++ src/main/java/br/com/leandrorodrigues/gestao_vagas/modules/candidate/useCases/ListAllJobsByFilterUseCase.java
@@ -1,17 +1,19 @@
-package br.com.leandrorodrigues.gestao_vagas.modules.company.useCases;
+package br.com.leandrorodrigues.gestao_vagas.modules.candidate.useCases;
 
 import br.com.leandrorodrigues.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.leandrorodrigues.gestao_vagas.modules.company.repositories.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class CreateJobUseCase {
+public class ListAllJobsByFilterUseCase {
 
     @Autowired
     private JobRepository jobRepository;
 
-    public JobEntity execute(JobEntity jobEntity) {
-        return this.jobRepository.save(jobEntity);
+    public List<JobEntity> execute(String filter) {
+        return this.jobRepository.findByDescriptionContaining(filter);
     }
 }
