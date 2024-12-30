1962466183f696b69d5fc7d01f512115441e2bf0
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2024-12-30 08:59:51 -0300

    feat: configuring swagger

--- src/main/java/br/com/leandrorodrigues/gestao_vagas/GestaoVagasApplication.java
+++ src/main/java/br/com/leandrorodrigues/gestao_vagas/GestaoVagasApplication.java
@@ -1,13 +1,21 @@
 package br.com.leandrorodrigues.gestao_vagas;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Job Management",
+                description = "API responsible for job management"
+        )
+)
 public class GestaoVagasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GestaoVagasApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoVagasApplication.class, args);
+    }
 
 }
