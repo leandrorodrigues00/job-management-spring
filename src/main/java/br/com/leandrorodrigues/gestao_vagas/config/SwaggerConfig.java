201f65c9cc935a7f56ffaa20b3bb95f7f3578654
Leandro Rodrigues <leandro.rodrigues00@hotmail.com>
2024-12-30 11:55:12 -0300

    feat: creating swaggerConfig

---
+++ src/main/java/br/com/leandrorodrigues/gestao_vagas/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package br.com.leandrorodrigues.gestao_vagas.config;
+
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        return new OpenAPI()
+                .info(
+                        new Info().title("Job Management")
+                                .description("API responsible for job management")
+                                .version("1")
+                )
+                .schemaRequirement("jwt_auth", creaSecurityScheme());
+    }
+
+    private SecurityScheme creaSecurityScheme() {
+        return new SecurityScheme().name("jwt_auth").type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT");
+    }
+}
